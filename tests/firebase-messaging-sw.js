01ef58734d411d775e5e2132e50329da167b7bf5
apjakjb <apjakjatiyavidyalaya@gmail.com>
2026-06-26 12:44:03 +0530

    Update firebase-messaging-sw.js

--- tests/firebase-messaging-sw.js
+++ tests/firebase-messaging-sw.js
@@ -35,7 +35,7 @@ messaging.onBackgroundMessage((payload) => {
 // =========================================================================
 // PWA CACHING LOGIC STARTS HERE (Tumhara purana code neeche rahega)
 // =========================================================================
-const CACHE_VERSION = 'premium-portal-v48';
+const CACHE_VERSION = 'premium-portal-v49';
 const STATIC_CACHE_NAME = `static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE_NAME = `dynamic-${CACHE_VERSION}`;
 
